fd955fd3af74a901ab5d825f926db807735b583c
Paula Grumazescu <paula@JarvisL.local>
2019-10-13 22:56:09 +0300

    Sem 2

---
+++ app/src/main/java/eu/ase/ro/damapp/AddPlayerActvity.java
@@ -0,0 +1,14 @@
+package eu.ase.ro.damapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddPlayerActvity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_player_actvity);
+    }
+}
